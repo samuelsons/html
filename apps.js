505cb0f4d49c6319a3a93afec7f91ac49fc5c85d
Samuel Osoba <samuelsontech@gmail.com>
2022-01-28 11:04:35 +0100

    Javascript hacks

--- apps.js
+++ apps.js
@@ -1,5 +1,65 @@
-//Function expression
+//Anon Function
+// const doctorize = function (firstName) {
+//   return `Dr. {firstName}`;
+// };
 
-const doctorize = function (firstName) {
-  return `Dr. {firstName}`;
-};
+//Function expression :: Variable Stored function.
+// const doctorize = function (firstName) {
+//   doesntExist();
+//   return `Dr. ${firstName}`;
+// };
+
+//Hoisting
+// console.log(doctorize3("Samuelsons"));
+
+// const doctorize = function (firstName) {
+//   doesntExist();
+//   return `Dr. ${firstName}`;
+// };
+// const doctorize = function (firstName) {
+//   doesntExist();
+//   return `Dr. ${firstName}`;
+// };
+
+// function doctorize3(firstName) {
+//   return `Dr. ${firstName}`;
+// }
+
+//Function declaration
+// function inchtoCM(inches) {
+//   return (cm = inches * 2.54);
+// }
+
+//anonymous function :: function without a function name.
+// const inchtoCM = function (inches) {
+//   return (cm = inches * 2.54);
+// };
+
+// Arrow function
+// Remove function & return keywords and also remove curly braces.
+
+// const inchtoCM = (inches) => (cm = inches * 2.54);
+
+// function add(a, b = 3) {
+//   const total = a + b;
+//   return total;
+// }
+// const add = (a, b = 3) => a + b;
+
+// returning an object
+
+function makeAbaby(first, last) {
+  const baby = {
+    name: `${first} ${last}`,
+    age: 0,
+  };
+  return baby;
+}
+
+// IIFE
+// Immediately Invoked Function Express
+
+(function () {
+  console.log("Running the anon function.");
+  return `Your are cool and your age is ${age}`;
+})(25);
